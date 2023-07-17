4506be9a477f0a3fda09ae106853200312289089
cotn <q003772003@gmail.com>
2023-07-17 15:17:43 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue-pug/vue3-recommended',
+    'eslint:recommended',
+    '@vue/standard'
+  ],
+  rules: {
+    'vue/max-attributes-per-line': 0
+  }
+}
